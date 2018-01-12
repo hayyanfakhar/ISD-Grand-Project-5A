12a8fe87b43a65ea619c4033e0920e1c890c6261
Bismah <bismah.babar1@gmail.com>
2018-01-12 09:41:24 +0500

    no message

---
+++ Crystal Report/WindowsFormsApplication5/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CrystalDecisions.CrystalReports.Engine;
+
+namespace WindowsFormsApplication5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+         
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ReportDocument cryRpt = new ReportDocument();
+            cryRpt.Load(@"C:\Users\Bismah\Documents\visual studio 2015\Projects\WindowsFormsApplication5\WindowsFormsApplication5\CrystalReport1.rpt");
+            crystalReportViewer1.ReportSource = cryRpt;
+            crystalReportViewer1.Refresh();
+        }
+    }
+}
